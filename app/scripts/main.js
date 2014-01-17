37f371cf20e939c4a0bd8f8776ee585f3688ce6c
Eric Menendez <ericmenendez@gmail.com>
2014-01-17 10:24:25 -0500

    make map responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,47 @@
-var mapId = '#map';
+var map = d3.select('#map');
 
-var width = $(mapId).width(),
-    height = 500;
+var mapRatio = 0.5;
+var width, height;
 
 var projection = d3.geo.conicConformal()
     .rotate([98, 0])
     .center([0, 38])
     .parallels([29.5, 45.5])
-    .scale(1000)
-    .translate([width / 2, height / 2])
     .precision(.1);
 
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = d3.select(mapId).append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = map.append("svg");
+
+function resize() {
+
+	width = parseInt(map.style('width'));
+	height = width * mapRatio;
+
+	projection
+		.scale(width)
+		.translate([width / 2, height / 2]);
+	
+    svg
+    	.attr("width", width)
+    	.attr("height", height);
+
+	d3.select(self.frameElement).style("height", height + "px");
+
+	svg.select('.land').attr('d', path);
+	
+}
+
+d3.select(window).on('resize', resize);
+
+resize();
 
 d3.json("../data/usa.json", function(error, us) {
+
   svg.append("path")
       .datum(topojson.feature(us, us.objects.subunits))
       .attr("class", "land")
       .attr("d", path);
 
 });
-
-d3.select(self.frameElement).style("height", height + "px");
