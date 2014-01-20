0f37aa96f1ac39054f290664c69b1db5c6182d14
Eric Menendez <ericmenendez@gmail.com>
2014-01-20 14:38:55 -0500

    Add raster background without resizing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,27 @@
 var map = d3.select('#map');
 
-var mapRatio = 0.5;
-var width, height;
+var reliefRatio = 734 / 1170;
+var lonRotate = 100;
+var latRotate = -45;
+var lonCenter = -97.74;
+var latTop = 51.75;
+var scaleRatio = 1297 / 956.4032697547684;
 
-var projection = d3.geo.conicConformal()
-    .rotate([98, 0])
-    .center([0, 38])
-    .parallels([29.5, 45.5])
-    .precision(.1);
+var width = 1140;
+var height = width * reliefRatio;
+
+var projection = d3.geo.azimuthalEqualArea()
+    .rotate([lonRotate, latRotate])
+    .center([lonRotate + lonCenter, latRotate + latTop])
+    .translate([width / 2, 0])
+    .scale(width * scaleRatio);
 
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = map.append("svg");
+var svg = map.append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 function resize() {
 
@@ -22,6 +31,10 @@ function resize() {
 	projection
 		.scale(width)
 		.translate([width / 2, height / 2]);
+
+	svg.select('#relief')
+		.attr('width', width)
+		.attr('height', height);
 	
     svg
     	.attr("width", width)
@@ -29,19 +42,37 @@ function resize() {
 
 	d3.select(self.frameElement).style("height", height + "px");
 
-	svg.select('.land').attr('d', path);
+	svg.select('#land').attr('d', path);
 	
 }
 
-d3.select(window).on('resize', resize);
+//d3.select(window).on('resize', resize);
 
-resize();
+//resize();
 
-d3.json("../data/usa.json", function(error, us) {
+d3.json("../data/usboundary.json", function(error, us) {
+	var defs = svg.append('defs');
 
-  svg.append("path")
-      .datum(topojson.feature(us, us.objects.subunits))
-      .attr("class", "land")
-      .attr("d", path);
+	defs.append("path")
+		.attr("id", "land")
+		.datum(topojson.feature(us, us.objects.layer1))
+		.attr("d", path);
+
+    svg.append('clipPath')
+    	.attr('id', 'clip')
+    	.append('use')
+    	.attr('xlink:href', '#land');
+
+    svg.append("image")
+    	.attr('id', 'relief')
+		//.attr("clip-path", "url(#clip)")
+		.attr("xlink:href", "../data/relief.png")
+		.attr("width", width)
+		.attr("height", height);
+
+	svg.append("use")
+		.attr("xlink:href", "#land");
 
 });
+
+d3.select(self.frameElement).style("height", height + "px");
