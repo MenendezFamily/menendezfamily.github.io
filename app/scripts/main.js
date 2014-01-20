59e99d1869621b7d3a2f2f6ffe2c5076f7418580
Eric Menendez <ericmenendez@gmail.com>
2014-01-20 14:43:44 -0500

    Add responsive resizing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,50 +7,42 @@ var lonCenter = -97.74;
 var latTop = 51.75;
 var scaleRatio = 1297 / 956.4032697547684;
 
-var width = 1140;
-var height = width * reliefRatio;
+var width, height;
 
-var projection = d3.geo.azimuthalEqualArea()
-    .rotate([lonRotate, latRotate])
-    .center([lonRotate + lonCenter, latRotate + latTop])
-    .translate([width / 2, 0])
-    .scale(width * scaleRatio);
+var projection = d3.geo.azimuthalEqualArea();
 
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = map.append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = map.append("svg");
 
 function resize() {
 
 	width = parseInt(map.style('width'));
-	height = width * mapRatio;
+	height = width * reliefRatio;
 
 	projection
-		.scale(width)
-		.translate([width / 2, height / 2]);
+	    .rotate([lonRotate, latRotate])
+	    .center([lonRotate + lonCenter, latRotate + latTop])
+	    .translate([width / 2, 0])
+	    .scale(width * scaleRatio);
+
+    svg
+    	.attr("width", width)
+    	.attr("height", height);
 
 	svg.select('#relief')
 		.attr('width', width)
 		.attr('height', height);
 	
-    svg
-    	.attr("width", width)
-    	.attr("height", height);
-
 	d3.select(self.frameElement).style("height", height + "px");
 
 	svg.select('#land').attr('d', path);
 	
 }
 
-//d3.select(window).on('resize', resize);
-
-//resize();
-
 d3.json("../data/usboundary.json", function(error, us) {
+	
 	var defs = svg.append('defs');
 
 	defs.append("path")
@@ -66,13 +58,13 @@ d3.json("../data/usboundary.json", function(error, us) {
     svg.append("image")
     	.attr('id', 'relief')
 		//.attr("clip-path", "url(#clip)")
-		.attr("xlink:href", "../data/relief.png")
-		.attr("width", width)
-		.attr("height", height);
+		.attr("xlink:href", "../data/relief.png");
 
 	svg.append("use")
 		.attr("xlink:href", "#land");
 
-});
+	d3.select(window).on('resize', resize);
 
-d3.select(self.frameElement).style("height", height + "px");
+	resize();
+
+});
