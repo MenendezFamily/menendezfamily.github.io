743e6d69c8812eab297084578584e78cf333e252
Eric Menendez <ericmenendez@gmail.com>
2014-01-21 14:10:24 -0500

    More styling and reorganize svg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,45 +19,72 @@ var path = d3.geo.path()
     .projection(projection);
 
 var svg = map.append("svg");
+var defs = svg.append('defs');
+
+function display() {
+	if (reliefReady && atReady && momdadReady) {
+		// Add paths to map
+		svg.append("use")
+			.attr('class', 'momdad')
+			.attr("xlink:href", "#momdad");
+
+		svg.append("use")
+			.attr('class', 'at')
+			.attr("xlink:href", "#at");
+
+		// Add resize event handler and run for the first time
+		d3.select(window).on('resize', resize);
+
+		resize();
+	}
+}
 
 function resize() {
 
-	if (reliefReady) {
+	width = parseInt(map.style('width'));
+	height = width * reliefRatio;
 
-		width = parseInt(map.style('width'));
-		height = width * reliefRatio;
+	projection
+	    .rotate([lonRotate, latRotate])
+	    .center([lonRotate + lonCenter, latRotate + latTop])
+	    .translate([width / 2, 0])
+	    .scale(width * scaleRatio);
 
-		projection
-		    .rotate([lonRotate, latRotate])
-		    .center([lonRotate + lonCenter, latRotate + latTop])
-		    .translate([width / 2, 0])
-		    .scale(width * scaleRatio);
+	svg
+		.attr("width", width)
+		.attr("height", height);
 
-		svg
-			.attr("width", width)
-			.attr("height", height);
+	svg.select('.relief')
+		.attr('width', width)
+		.attr('height', height);
 
-		svg.select('.relief')
-			.attr('width', width)
-			.attr('height', height);
+	d3.select(self.frameElement).style("height", height + "px");
 
-		d3.select(self.frameElement).style("height", height + "px");
+	svg.select('#land').attr('d', path);
 
-		svg.select('#land').attr('d', path);
-
-		if (atReady) svg.select('.at').attr('d', path);
-		if (momdadReady) svg.select('.momdad').attr('d', path);
+	svg.select('#at').attr('d', path);
+	svg.select('#momdad').attr('d', path);
 		
-	}
 }
 
 d3.json("../data/us.json", function(error, us) {
 
-	var defs = svg.append('defs');
-
 	defs.append("path")
 		.attr("id", "land")
 		.datum(topojson.feature(us, us.objects.land));
+
+	defs.append('filter')
+		.attr('id', 'desat')
+		.append('feColorMatrix')
+			.attr('in', 'SourceGraphic')
+			.attr('type', 'saturate')
+			.attr('values', '0.3');
+
+	defs.append('filter')
+		.attr('id', 'glow')
+		.append('feGaussianBlur')
+			.attr('in', 'SourceGraphic')
+			.attr('stdDeviation', '5');
 
     svg.append('clipPath')
     	.attr('id', 'clip')
@@ -67,6 +94,7 @@ d3.json("../data/us.json", function(error, us) {
     svg.insert("image", ":first-child")
     	.attr('class', 'relief')
 		.attr("clip-path", "url(#clip)")
+		.attr("filter", "url(#desat)")
 		.attr("xlink:href", "../data/relief.png");
 
 	svg.append("use")
@@ -74,32 +102,33 @@ d3.json("../data/us.json", function(error, us) {
 
 	reliefReady = true;
 
-	d3.select(window).on('resize', resize);
-
-	resize();
+	display();
 
 });
 
 d3.json("../data/at.json", function(error, at) {
 
-	svg.append('path')
-		.attr('class', 'at')
+		// .attr('class', 'glow')
+		// .attr('filter', 'url(#glow)')
+
+	defs.append('path')
+		.attr('id', 'at')
 		.datum(topojson.feature(at, at.objects.at));
 
 	atReady = true;
 
-	resize();
+	display();
 
 });
 
 d3.json("../data/momdad.json", function(error, momdad) {
 
-	svg.append('path')
-		.attr('class', 'momdad')
+	defs.append('path')
+		.attr('id', 'momdad')
 		.datum(topojson.feature(momdad, momdad.objects.momdad));
 
 	momdadReady = true;
 
-	resize();
+	display();
 
 });
