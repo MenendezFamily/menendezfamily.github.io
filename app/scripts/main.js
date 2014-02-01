93aa9d3098dcd3f4bef2e4c2fecab78445193f33
Eric Menendez <ericmenendez@gmail.com>
2014-02-01 10:20:50 -0500

    First stab at canvas rendering, with test animated rotation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,69 +9,97 @@ var maxWidthRatio = 0.6;
 var maxHeightRatio = 1.3;
 var patternSide = 400;
 
-var width, height;
+var width = 0, height = 0;
+
+// Colors from ColorBrewer2
+var colors = {
+    'red':      '#e41a1c',
+    'blue':     '#377eb8',
+    'green':    '#4daf4a',
+    'purple':   '#984ea3',
+    'orange':   '#ff7f00',
+    'yellow':   '#ffff33',
+    'brown':    '#a65628',
+    'pink':     '#f781bf',
+};
+
+// Display variables
+var defaultStrokeWidth = '3',
+    mouseoverStrokeWidth = '5',
+    transitionDuration = 300,
+    blurRadius = 3;
 
 // Data loading
 var dataToLoad = {
-    'at': 'at/',
-    'at_buffer': null,
-    'momdad': 'http://mannymarsha.wordpress.com/',
-    'momdad_buffer': null,
-    'transam': 'http://picasaweb.com/nathanbiketrip',
-    'transam_buffer': null,
-    'camino': 'http://blog.travelpod.com/travel-blog/aggiesontheway/2/tpod.html',
-    'camino_buffer': null,
+    'at': {
+        'url': 'at/',
+        'color': colors['brown'],
+    },
+    'momdad': {
+        'url': 'http://mannymarsha.wordpress.com/',
+        'color': colors['purple'],
+    },
+    'transam': {
+        'url': 'http://picasaweb.com/nathanbiketrip',
+        'color': colors['green'],
+    },
+    'camino': {
+        'url': 'http://blog.travelpod.com/travel-blog/aggiesontheway/2/tpod.html',
+        'color': colors['red'],
+    },
 };
 var dataReadyCount = 0;
 var baseReady = false;
 
-// Display variables
-var defaultStrokeWidth = '3';
-var mouseoverStrokeWidth = '5';
-var transitionDuration = 300;
-var blurRadius = 3;
+var map = d3.select('#map');
+// var svg = map.append('svg');
+// var defs = svg.append('defs');
+var canvas = map.append("canvas")  
 
-var projection = d3.geo.orthographic()
-    .clipAngle(90)
-    .precision(.1);
-
-var path = d3.geo.path()
-    .projection(projection);
+var c = canvas.node().getContext("2d");
 
 var graticule = d3.geo.graticule();
 
-var map = d3.select('#map');
-var svg = map.append('svg');
-var defs = svg.append('defs');
+var projection = d3.geo.orthographic()
+    .clipAngle(90);
 
-svg.append("path")
-    .datum(graticule)
-    .attr("id", "graticule");
+var path = d3.geo.path()
+    .projection(projection)
+    .context(c);
+
+// svg.append("path")
+//     .datum(graticule)
+//     .attr("id", "graticule");
+//     
 
 //
 // Load topjson data
 // 
-d3.json('../data/world.json', function (error, world) {
+var land;
+d3.json('../data/world-110m.json', function (error, world) {
 
-    defs.append('path')
-        .attr('id', 'land')
-        .datum(topojson.feature(world, world.objects.world));
+    // defs.append('path')
+    //     .attr('id', 'land')
+    //     .datum(topojson.feature(world, world.objects.world));
 
-    defs.append('pattern')
-        .attr('id', 'pattern')
-        .attr('patternUnits', 'userSpaceOnUse')
-        .attr('width', patternSide)
-        .attr('height', patternSide)
-        .append('image')
-            .attr('xlink:href', '../images/mochaGrunge.png')
-            .attr('x', '0')
-            .attr('y', '0')
-            .attr('width', patternSide)
-            .attr('height', patternSide);
+    // defs.append('pattern')
+    //     .attr('id', 'pattern')
+    //     .attr('patternUnits', 'userSpaceOnUse')
+    //     .attr('width', patternSide)
+    //     .attr('height', patternSide)
+    //     .append('image')
+    //         .attr('xlink:href', '../images/mochaGrunge.png')
+    //         .attr('x', '0')
+    //         .attr('y', '0')
+    //         .attr('width', patternSide)
+    //         .attr('height', patternSide);
 
-    svg.append('use')
-        .attr('xlink:href', '#land')
-        .attr('fill', 'url(#pattern)');
+    // svg.append('use')
+    //     .attr('xlink:href', '#land')
+    //     .attr('fill', 'url(#pattern)');
+    //     
+    
+    land = topojson.feature(world, world.objects.land);
 
     baseReady = true;
 
@@ -81,9 +109,7 @@ d3.json('../data/world.json', function (error, world) {
 
 function loadPathCallback(key) {
     return function (error, data) {
-        defs.append('path')
-            .attr('id', key)
-            .datum(topojson.feature(data, eval('data.objects.' + key)));
+        dataToLoad[key]['feature'] = topojson.feature(data, eval('data.objects.' + key));
         dataReadyCount++;
         display();
     };
@@ -91,62 +117,72 @@ function loadPathCallback(key) {
 
 for (var key in dataToLoad) {
     d3.json('../data/' + key + '.json', loadPathCallback(key));
+    // TODO: also load buffer
 }
 
 function display() {
-    if (baseReady && dataReadyCount === Object.keys(dataToLoad).length) {
+   if (baseReady && dataReadyCount === Object.keys(dataToLoad).length) {
         // Add paths to map
         function drawPath(pathName) {
-            var g = svg.append('a')
-                        .attr('xlink:href', dataToLoad[pathName])
-                        .append('g')
-                            .attr('class', 'path-group ' + pathName);
+            // var g = svg.append('a')
+            //             .attr('xlink:href', dataToLoad[pathName])
+            //             .append('g')
+            //                 .attr('class', 'path-group ' + pathName);
 
-            var path = g.append('use')
-                .attr('class', 'path')
-                .style('stroke-width', defaultStrokeWidth)
-                .attr('xlink:href', '#' + pathName);
+            // var path = g.append('use')
+            //     .attr('class', 'path')
+            //     .style('stroke-width', defaultStrokeWidth)
+            //     .attr('xlink:href', '#' + pathName);
 
-            var link = d3.select('.copy .' + pathName);
-            link.style('transition-duration', transitionDuration + 'ms');
+            // var link = d3.select('.copy .' + pathName);
+            // link.style('transition-duration', transitionDuration + 'ms');
 
-            g.append('use')
-                .attr('class', 'buffer')
-                .attr('xlink:href', '#' + pathName + '_buffer');
+            // g.append('use')
+            //     .attr('class', 'buffer')
+            //     .attr('xlink:href', '#' + pathName + '_buffer');
 
-            function setPathStroke(width) {
-                path.transition().duration(transitionDuration).style('stroke-width', width);
-            }
+            // function setPathStroke(width) {
+            //     path.transition().duration(transitionDuration).style('stroke-width', width);
+            // }
 
-            function setTextShadow(width) {
-                var textShadow = '0px 0px ' + width + 'px';
-                if (width > 0) {
-                    textShadow += ' ' + path.style('stroke');
-                }
-                link.style('text-shadow', textShadow);
-            }
+            // function setTextShadow(width) {
+            //     var textShadow = '0px 0px ' + width + 'px';
+            //     if (width > 0) {
+            //         textShadow += ' ' + path.style('stroke');
+            //     }
+            //     link.style('text-shadow', textShadow);
+            // }
 
-            function doMouseover() {
-                setPathStroke(mouseoverStrokeWidth);
-                setTextShadow(blurRadius);
-            }
+            // function doMouseover() {
+            //     setPathStroke(mouseoverStrokeWidth);
+            //     setTextShadow(blurRadius);
+            //     d3.transition()
+            //         .tween("rotate", function() {
+            //             r = d3.interpolate(projection.rotate(), [100, latRotate]);
+            //             return function(t) {
+            //                 projection.rotate(r(t));
+            //                 svg.select('#momdad').attr('d', window.path);
+            //             };
+            //         })
+            //         .transition();
+            // }
 
-            function doMouseout() {
-                setPathStroke(defaultStrokeWidth);
-                setTextShadow(0);
-            }
+            // function doMouseout() {
+            //     setPathStroke(defaultStrokeWidth);
+            //     setTextShadow(0);
+            // }
 
-            g.on('mouseover', doMouseover);
-            link.on('mouseover', doMouseover);
+            // g.on('mouseover', doMouseover);
+            // link.on('mouseover', doMouseover);
 
-            g.on('mouseout', doMouseout);
-            link.on('mouseout', doMouseout);
+            // g.on('mouseout', doMouseout);
+            // link.on('mouseout', doMouseout);
         }
 
-        drawPath('momdad');
-        drawPath('transam');
-        drawPath('at');
-        drawPath('camino');
+        // drawPath('momdad');
+        // drawPath('transam');
+        // drawPath('at');
+        // drawPath('camino');
 
         // Add resize event handler and run for the first time
         d3.select(window).on('resize', resize);
@@ -155,7 +191,10 @@ function display() {
 }
 
 function resize() {
-    // Ensure entire page is always visible
+    // Clear whatever is currently drawn
+    c.clearRect(0, 0, width, height);
+
+    // Calculate dimensions to ensure entire page is always visible
     var nonMapHeight = parseInt(d3.select('.container').style('height')) +
                        parseInt(d3.select('#footer').style('height')) +
                        5;
@@ -175,16 +214,54 @@ function resize() {
         .scale(scale)
         .translate([width / 2, height]);
 
-    svg
-        .attr('width', width)
-        .attr('height', height);
+    // svg
+    //     .attr('width', width)
+    //     .attr('height', height);
+    //     
+
+    canvas
+        .attr("width", width)  
+        .attr("height", height);
 
     d3.select(self.frameElement).style('height', height + 'px');
 
-    svg.select('#graticule').attr('d', path);
-    svg.select('#land').attr('d', path);
+    // svg.select('#graticule').attr('d', path);
+    // svg.select('#land').attr('d', path);
+
+    // for (var key in dataToLoad) {
+    //     svg.select('#' + key).attr('d', path);
+    // }
+    
+
+    c.strokeStyle = '#000', c.lineWidth = 1, c.beginPath(), path(graticule()), c.stroke();
+    c.fillStyle = "#bbb", c.beginPath(), path(land), c.fill(), c.stroke();
 
     for (var key in dataToLoad) {
-        svg.select('#' + key).attr('d', path);
+        c.strokeStyle = dataToLoad[key]['color'], c.lineWidth = defaultStrokeWidth, c.beginPath(), path(dataToLoad[key]['feature']), c.stroke();
     }
+
+    //         c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
+    //         c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+    //         c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+
+}
+
+function testRotate(lon) {
+    d3.transition()
+        .duration(1000)
+        .tween("rotate", function() {
+            r = d3.interpolate(projection.rotate(), [lon, latRotate]);
+            return function(t) {
+                projection.rotate(r(t));
+                c.clearRect(0, 0, width, height);
+                c.strokeStyle = '#000', c.lineWidth = 1, c.beginPath(), path(graticule()), c.stroke();
+                c.fillStyle = "#bbb", c.beginPath(), path(land), c.fill(), c.stroke();
+
+                for (var key in dataToLoad) {
+                    c.strokeStyle = dataToLoad[key]['color'], c.lineWidth = defaultStrokeWidth, c.beginPath(), path(dataToLoad[key]['feature']), c.stroke();
+                }
+
+            };
+        })
+        .transition();
 }
